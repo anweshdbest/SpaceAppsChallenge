71bf7c0c8662fc7ac00493d0973a9fba4e78b70f
Scott Hunter <scott.k.hunter@gmail.com>
2013-04-20 19:03:28 -0400

    refactor CZML loading, fix #15.

--- src/app.js
+++ src/app.js
@@ -21,11 +21,6 @@ define(function(require) {
         centralBody.logoOffset = new Cesium.Cartesian2(300, 26);
 
         var clock = widget.clock;
-        clock.currentTime = Cesium.JulianDate.fromIso8601('20110725T1843Z');
-        clock.startTime = clock.currentTime;
-        clock.stopTime = clock.currentTime.addDays(4);
-        clock.multiplier = 1;
-        clock.clockRange = Cesium.ClockRange.LOOP_STOP;
 
         var clockViewModel = new Cesium.ClockViewModel(clock);
 
@@ -60,36 +55,54 @@ define(function(require) {
         var baseLayerPicker = new Cesium.BaseLayerPicker('baseLayerPickerContainer', imageryLayers, imageryProviderViewModels);
         baseLayerPicker.viewModel.selectedItem(imageryProviderViewModels[0]);
 
-        var issObjectCollection = new Cesium.DynamicObjectCollection();
-        var issVisualizers = new Cesium.VisualizerCollection(Cesium.CzmlDefaults.createVisualizers(scene), issObjectCollection);
-
         var photoObjectCollection = new Cesium.DynamicObjectCollection();
         var photoVisualizers = new Cesium.VisualizerCollection([new Cesium.DynamicPolygonBatchVisualizer(scene)], photoObjectCollection);
-        function loadPhotoCzml(url) {
-            photoVisualizers.removeAllPrimitives();
+
+        var issObjectCollection = new Cesium.DynamicObjectCollection();
+        var issVisualizers;
+
+        var currentMissionName;
+        function loadCzml(missionName) {
+            if (currentMissionName === missionName) {
+                return;
+            }
+
+            var photoUrl = require.toUrl('../Assets/CZML/' + missionName + '.czml');
+            var issUrl = require.toUrl('../Assets/CZML/' + missionName + '_ISS.czml');
+
             photoObjectCollection.clear();
-
-            return Cesium.loadJson(url).then(function(czml) {
-                Cesium.processCzml(czml, photoObjectCollection, url);
-                photoVisualizers.update(Cesium.Iso8601.MINIMUM_VALUE);
-            });
-        }
-
-        function loadIssCzml(url) {
-            issVisualizers.removeAllPrimitives();
             issObjectCollection.clear();
 
-            return Cesium.loadJson(url).then(function(czml) {
-                Cesium.processCzml(czml, issObjectCollection, url);
+            if (typeof issVisualizers !== 'undefined') {
+                issVisualizers = issVisualizers.destroy();
+            }
+
+            Cesium.when.all([Cesium.loadJson(photoUrl), Cesium.loadJson(issUrl)]).then(function(czmlArray) {
+                var photoCzml = czmlArray[0];
+                var issCzml = czmlArray[1];
+
+                Cesium.processCzml(photoCzml, photoObjectCollection, photoUrl);
+                photoVisualizers.update(Cesium.Iso8601.MINIMUM_VALUE);
+
+                Cesium.processCzml(issCzml, issObjectCollection, issUrl);
+                issVisualizers = new Cesium.VisualizerCollection(Cesium.CzmlDefaults.createVisualizers(scene), issObjectCollection);
+
+                var document = issObjectCollection.getObject('document');
+                if (typeof document !== 'undefined' && typeof document.clock !== 'undefined') {
+                    clock.startTime = document.clock.startTime;
+                    clock.stopTime = document.clock.stopTime;
+                    clock.clockRange = document.clock.clockRange;
+                    clock.clockStep = document.clock.clockStep;
+                    clock.multiplier = document.clock.multiplier;
+                    clock.currentTime = document.clock.currentTime;
+
+                    timelineWidget.zoomTo(clock.startTime, clock.stopTime);
+                    clockViewModel.synchronize();
+                }
             });
         }
 
-        function loadCzml(missonName) {
-            loadPhotoCzml(require.toUrl('../Assets/CZML/' + missonName + '.czml'));
-            loadIssCzml(require.toUrl('../Assets/CZML/' + missonName + '_ISS.czml'));
-        }
-
-        loadCzml('ISS11_07_image_data');
+        loadCzml('ISS13_01_image_data');
 
         clock.onTick.addEventListener(function(time) {
             if (typeof issVisualizers !== 'undefined') {
